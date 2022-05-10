5dd3f951b783226d58c2999bb4d0d13addee8720
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2022-05-10 20:35:43 -0300

    Commit parcial da implementação da Classe DAL
    Fiz este commit para que o Wanderson pegasse o código, porque a máquina dele desligou e ele perdeu o código.

---
+++ Base/DAL/UsuarioDAL.cs
@@ -0,0 +1,36 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        public Usuario Inserir(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = "";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "SP_InserirUsuario";
+
+                SqlParameter pativo = new SqlParameter();
+                //TODO: Terminar o metodo
+                return new Usuario();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
