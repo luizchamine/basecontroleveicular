8469b3d18d25806f46da352654a2008f382d5f7c
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2022-05-13 21:50:16 -0300

    Formulário de cadastro de usuário já está fazendo insert no banco
    Foi implementado o FormCadastroUsuario e codificada a rotina para inserir os dados no banco.

--- Base/BLL/UsuarioBLL.cs
+++ Base/BLL/UsuarioBLL.cs
@@ -6,15 +6,10 @@ namespace BLL
 {
     public class UsuarioBLL
     {
-        public void Inserir()
+        public void Inserir(Usuario _usuario)
         {
-            Usuario usuario = new Usuario();
-            usuario.NomeUsuario = "João";
-            usuario.Senha = "asdf123";
-            usuario.Ativo = true;
-
             UsuarioDAL usuarioDAL = new UsuarioDAL();
-            usuarioDAL.Inserir(usuario);
+            usuarioDAL.Inserir(_usuario);
         }
         public void Excluir()
         {
