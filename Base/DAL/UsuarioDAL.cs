de16e86244aa8c8afc3516cac27cc8c644212110
Erisvaldo Carvalho Silva <erisvaldocarvalhosilva@gmail.com>
2022-05-18 22:04:57 -0300

    Implementação do CRUD na UsuarioDAL
    Implementado o método Buscar e Excluir. Iniciada a implementação do método Alterar.

--- Base/DAL/UsuarioDAL.cs
+++ Base/DAL/UsuarioDAL.cs
@@ -56,5 +56,95 @@ namespace DAL
                 cn.Close();
             }
         }
+        public DataTable Buscar(string _filtro)
+        {
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            SqlConnection cn = new SqlConnection();
+
+            try
+            {
+                cn.ConnectionString = @"User ID=Erisvaldo;Initial Catalog=Loja;Data Source=.\SQLEXPRESS2019;Password=123";
+                SqlCommand cmd = new SqlCommand();
+                da.SelectCommand = cmd;
+                da.SelectCommand.Connection = cn;
+                da.SelectCommand.CommandText = "SP_BuscarUsuario";
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                SqlParameter pfiltro = new SqlParameter("@filtro", SqlDbType.VarChar);
+                pfiltro.Value = _filtro;
+                da.Fill(dt);
+                return dt;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public void Excluir(int _id)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = "";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_ExcluirUsuario";
+                SqlParameter pid = new SqlParameter();
+                pid.Value = _id;
+                cmd.Parameters.Add(pid);
+
+                cn.Open();
+                int resultado = cmd.ExecuteNonQuery();
+                if (resultado != 1)
+                    throw new Exception("Não possivel excluir o usuário: " + _id.ToString());
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public Usuario Alterar(Usuario _usuario)
+        {
+            SqlConnection cn = new SqlConnection();
+            try
+            {
+                cn.ConnectionString = "";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_AlterarUsuario";
+
+                return _usuario;
+            }
+            catch(SqlException ex)
+            {
+                throw new Exception("Servidor SQL Erro: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
